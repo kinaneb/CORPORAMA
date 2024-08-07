dc057dfcfd20840e883533c4bc256537d879f7dd
Kinan <kinanb@live.com>
2024-08-07 22:33:36 +0200

    implementing the basic algorithm

--- app/app.py
+++ app/app.py
@@ -4,9 +4,22 @@ def analyze(file_path):
     try:
         with open(file_path, 'r') as file:
             sirens = file.read().splitlines()
-        return sirens
+            sirens.sort()
+            previous = ""
+            next = ""
+            unique = []
+            other = []
+            for i in range(1, len(sirens)-1):
+                previous = sirens[i-1]
+                current = sirens[i]
+                next = sirens[i+1]
+                if current != previous and current != next:
+                    unique.append(current)
+                else:
+                    other.append(current)
+        return len(unique), len(other)
     except Exception as e:
-        return str(e)
+        return str(e), None
     
 def main():
     if len(sys.argv) < 2:
