8d9cd14712f5c2404e1b22cdb3063e248d9622fe
Kinan <kinanb@live.com>
2024-08-08 17:00:34 +0200

    fix algorithm

--- front/analyzer.js
+++ front/analyzer.js
@@ -2,19 +2,35 @@ $(document).ready(function() {
     function analyze() {
         var sirens_occurrences = {};
         var siren_li_elements = {};
+        var sirens_occurrences_counter = 0;
+
         $('li').each(function() {
             var siren = $(this).text().trim();
-            if(undefined === siren_li_elements[siren]) {
-                siren_li_elements[siren] = [this];
-                sirens_occurrences[siren] = 1;
-                $(this).addClass('unique');
+            if (siren.length === 0) {
+                return;
             }
-            else {
+            if(sirens_occurrences[siren]) {
                 siren_li_elements[siren].push(this);
                 sirens_occurrences[siren]++;
-                $(this).addClass('other');
+            }
+            else {
+                siren_li_elements[siren] = [this];
+                sirens_occurrences_counter++;
+                sirens_occurrences[siren] = 1;
             }
         });
+        var unique_siren_counter = 0;
+        $.each(sirens_occurrences, function(siren, occurrence) {
+            var elements = siren_li_elements[siren];
+            if (occurrence === 1) {
+                unique_siren_counter++;
+                $(elements).addClass('unique');
+            } else {
+                $(elements).addClass('other');
+            }
+        });
+        console.log('Unique sirens:', unique_siren_counter);
+        console.log('Non unique sirens:', sirens_occurrences_counter - unique_siren_counter);
     }
     analyze();
 });
